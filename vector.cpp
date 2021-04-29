94b07fb64a42fa93444ed13476b8608a796ae756
Provence Pipes <ppipes@ga-v5.msk.21-school.ru>
2021-04-29 19:15:19 +0300

    0

--- vector.cpp
+++ vector.cpp
@@ -1,20 +1,31 @@
 #include <vector>
+#include <string>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Как же всё-таки работает вектор..? 🤔🤔🤔" << std::endl;
-    std::vector<int> v;
-    std::cout << "++" << std::endl;
-    for(int i = 0; i != 18; i++)
-    {
-        v.push_back(42);
-        std::cout << "SIZE: " << v.size() << " CAPACITY: " << v.capacity() << std::endl;
-    }
-    std::cout << "--" << std::endl;
-    for(int i = 0; i != 5; i++)
-    {
-        v.pop_back();
-        std::cout << "SIZE: " << v.size() << " CAPACITY: " << v.capacity() << std::endl;
-    }
+    std::vector<std::string> v;
+    // std::cout << "++" << std::endl;
+    // for(int i = 0; i != 18; i++)
+    // {
+    //     v.push_back(42);
+    //     std::cout << "SIZE: " << v.size() << " CAPACITY: " << v.capacity() << std::endl;
+    // }
+    // std::cout << "--" << std::endl;
+    // for(int i = 0; i != 5; i++)
+    // {
+    //     v.pop_back();
+    //     std::cout << "SIZE: " << v.size() << " CAPACITY: " << v.capacity() << std::endl;
+    // }
+
+    v.push_back("Blabla");
+    std::cout << v[0] << std::endl;
+
+    std::string *ptr = &v[0];
+
+    v.push_back("croak");
+    v.push_back("killkillkill");
+
+    std::cout << *ptr << std::endl;
+
 }
