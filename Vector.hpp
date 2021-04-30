ea4fdc2934222747d2173ef4f34a4683a6572f82
Provence Pipes <ppipes@ga-u3.msk.21-school.ru>
2021-04-30 20:49:06 +0300

    0

--- Vector.hpp
+++ Vector.hpp
@@ -1,119 +1,143 @@
 #include <memory>
+namespace ft {
 
 template <class T, class Alloc = std::allocator<T> >
-
 class vector
 {
 private:
-
 	T * _data;
+  Alloc _allocator;
 	int _capacity;
 	int _size;
 public:
-	// this is alias
-    typedef T value_type; // тип элемента
-    typedef Alloc allocator_type; // use in constructor
-    typedef reference allocator_type::reference; // то же что и value_type&
-    typedef const_reference allocator_type::const_reference; // const value_type&
-	typedef  pointer allocator_type::pointer;
-    typedef const_pointer allocator_type::const_pointer;
-    typedef iterator ; // struct random_access_iterator_tag to value_type  тип итератора для контейнера данного типа
-    typedef const_iterator ; // struct random_access_iterator_tag to const value_type
-    typedef reverse_iterator reverse_iterator<iterator>; // template <class Iterator> class reverse_iterator;
-    typedef const_reverse_iterator reverse_iterator<const_iterator>;
-    typedef int difference_type; // iterator_traits<iterator>::difference_type  значение разницы между двумя итераторами
-    typedef unsigned int size_type; // значение размера наибольшего возможного контейнера данного типа
+    typedef T value_type;
+    typedef Alloc allocator_type;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+	typedef  value_type* pointer;
+    typedef const value_type* const_pointer;
+//     typedef iterator ; // struct random_access_iterator_tag to value_type  тип итератора для контейнера данного типа
+//     typedef const_iterator ; // struct random_access_iterator_tag to const value_type
+//     typedef reverse_iterator reverse_iterator<iterator>; // template <class Iterator> class reverse_iterator;
+//     typedef const_reverse_iterator reverse_iterator<const_iterator>;
+    typedef ptrdiff_t difference_type; // iterator_traits<iterator>::difference_type  значение разницы между двумя итераторами
+    typedef size_t size_type; // значение размера наибольшего возможного контейнера данного типа
 
 public:
 
     /* CONSTRUCTORS && DESTRUCTOR*/
 
-    explicit vector (const allocator_type& alloc = allocator_type()); // default constructor, empty container
+    explicit vector (const allocator_type& alloc = allocator_type()) {
+      _allocator = alloc;
+      _data = NULL;
+      _size = 0;
+      _capacity = 0;
+    }
 
-    explicit vector (size_type n, const value_type& val = value_type(),
-                 const allocator_type& alloc = allocator_type()); // container with n elements, each element is a copy of val
+    explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) {
+      _allocator = alloc;
+      _data = _allocator.allocate(n);
+      pointer tmp = _data;
+      for(int i = 0; i < n; i++)
+      {
+          _allocator.construct(tmp, val);
+          tmp++;
+      }
+    //   pointer end = _data + n;
+    //   for(pointer i = _data; i != end; i++)
+    //   {
+    //     _allocator.construct(i, val);
+    //   }
+      
+      _size = n;
+      _capacity = n;
+    }
 
-    template <class InputIterator>
-    vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()); // range constructor, including first excluding last
+//     template <class InputIterator>
+//     vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()); // range constructor, including first excluding last
 
-    vector (const vector& x) { *this = x; } // copy constructor
+//     vector (const vector& x) { *this = x; } // copy constructor
 
-    ~vector();  // destroy elements and deallocate capacity
+//     ~vector();  // destroy elements and deallocate capacity
 
-    vector& operator= (const vector& x);
+//     vector& operator= (const vector& x);
 
-     /* ITERATORS */
+//      /* ITERATORS */
 
-    iterator begin(); // return random access iterator pointing to first element
-    const_iterator begin() const;
-    iterator end(); // return iterator reffering next after last element. iterator is alias
-    const_iterator end() const;
-    reverse_iterator rbegin(); // last element
-    const_reverse_iterator rbegin() const;
-    reverse_iterator rend(); // theoretical element before first
-    const_reverse_iterator rend() const;
-
-
-     /* CAPACITY */
-
-    size_type size() const { return _size; } // return number of elements (not equal capacity), size_type is alias for unsigned int
-    size_type max_size() const; // return maximum number of elements (potential), in reference is 1073741823
-    void resize (size_type n, value_type val = value_type()); // change size, if n < container reduces and destroy other elements, 
-	// if n > container expanded, if n > capacity reallocate storage
-    size_type capacity() const { return _capacity; } // ret size of allocated storage capacity (number elements), size_type = unsigned int. capacity can change by reserve
-    bool empty() const { return (_size == 0 ? 1 : 0); } // if size = 0 return true, else return false
-    void reserve (size_type n); // request change of capacity, if n < current capacity, do nothing
-
-
-    /* ELEMENT ACCESS */
-
-    reference operator[] (size_type n); // return reference to element at position n
-    const_reference operator[] (size_type n) const;
-    reference at (size_type n); // same as [], but can throw exception if out of range
-    const_reference at (size_type n) const;
-    reference front(); // return reference to first element, UB if empty vector. reference is alias
-    const_reference front() const; 
-    reference back(); // return reference to last element, UB if empty vector. reference is alias
-    const_reference back() const;
+//     iterator begin(); // return random access iterator pointing to first element
+//     const_iterator begin() const;
+//     iterator end(); // return iterator reffering next after last element. iterator is alias
+//     const_iterator end() const;
+//     reverse_iterator rbegin(); // last element
+//     const_reverse_iterator rbegin() const;
+//     reverse_iterator rend(); // theoretical element before first
+//     const_reverse_iterator rend() const;
 
 
-    /* MODIFIERS */
+//      /* CAPACITY */
 
-    template <class InputIterator>
-    void assign (InputIterator first, InputIterator last);	// replace content on range (destroy all old elements)
-    void assign (size_type n, const value_type& val); // replace content of n elements, each value is val
-    void push_back (const value_type& val); // add new element at the end, increase container size by 1
-    void pop_back(); // remove the last element, decrease size by 1
-    iterator insert (iterator position, const value_type& val); // insert new element before position
-    void insert (iterator position, size_type n, const value_type& val); // insert n elements before position
-    template <class InputIterator>
-    void insert (iterator position, InputIterator first, InputIterator last); // insert range elements before position
-    iterator erase (iterator position); // remove element on position
-    iterator erase (iterator first, iterator last); // remove range of elements
-    void swap (vector& x); // swap content
-    void clear(); // removes all elements, leaving container size = 0
+    size_type size() const { return _size; }
+//     size_type max_size() const; // return maximum number of elements (potential), in reference is 1073741823
+//     void resize (size_type n, value_type val = value_type()); // change size, if n < container reduces and destroy other elements, 
+// 	// if n > container expanded, if n > capacity reallocate storage
+    size_type capacity() const { return _capacity; }
+//     bool empty() const { return (_size == 0 ? 1 : 0); } // if size = 0 return true, else return false
+//     void reserve (size_type n); // request change of capacity, if n < current capacity, do nothing
 
-};
 
-/* NON-MEMBER OVERLOADS */
+//     /* ELEMENT ACCESS */
 
-template <class T, class Alloc>
-  bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs); // comparing sizes, if match - comparing elements
+    reference operator[] (size_type n) { return _data[n]; } // return reference to element at position n
+//     const_reference operator[] (size_type n) const;
+//     reference at (size_type n); // same as [], but can throw exception if out of range
+//     const_reference at (size_type n) const;
+//     reference front(); // return reference to first element, UB if empty vector. reference is alias
+//     const_reference front() const; 
+//     reference back(); // return reference to last element, UB if empty vector. reference is alias
+//     const_reference back() const;
 
-template <class T, class Alloc>
-  bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 
-template <class T, class Alloc>
-  bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs); // lexicographical_compare 
+//     /* MODIFIERS */
 
-template <class T, class Alloc>
-  bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+//     template <class InputIterator>
+//     void assign (InputIterator first, InputIterator last);	// replace content on range (destroy all old elements)
+//     void assign (size_type n, const value_type& val); // replace content of n elements, each value is val
+//     void push_back (const value_type& val); // add new element at the end, increase container size by 1
+//     void pop_back(); // remove the last element, decrease size by 1
+//     iterator insert (iterator position, const value_type& val); // insert new element before position
+//     void insert (iterator position, size_type n, const value_type& val); // insert n elements before position
+//     template <class InputIterator>
+//     void insert (iterator position, InputIterator first, InputIterator last); // insert range elements before position
+//     iterator erase (iterator position); // remove element on position
+//     iterator erase (iterator first, iterator last); // remove range of elements
+//     void swap (vector& x); // swap content
+//     void clear(); // removes all elements, leaving container size = 0
 
-template <class T, class Alloc>
-  bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+// };
 
-template <class T, class Alloc>
-  bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+// /* NON-MEMBER OVERLOADS */
 
-template <class T, class Alloc>
-  void swap (vector<T,Alloc>& x, vector<T,Alloc>& y); // containers exchange references to their data, without copy or move elements
+// template <class T, class Alloc>
+//   bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs); // comparing sizes, if match - comparing elements
+
+// template <class T, class Alloc>
+//   bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+
+// template <class T, class Alloc>
+//   bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs); // lexicographical_compare 
+
+// template <class T, class Alloc>
+//   bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+
+// template <class T, class Alloc>
+//   bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+
+// template <class T, class Alloc>
+//   bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+
+// template <class T, class Alloc>
+//   void swap (vector<T,Alloc>& x, vector<T,Alloc>& y); // containers exchange references to their data, without copy or move elements
+
+
+}; // class bracket
+} // namespace bracket
--- vector.cpp
+++ vector.cpp
@@ -1,10 +1,45 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "Vector.hpp"
 
 int main()
 {
-    std::vector<std::string> v;
+    std::cout << "TESTING CONSTRUCTORS:" << std::endl;
+    std::vector<std::string> v_empty;
+    std::cout << "std | Default constructor: size - " << v_empty.size() << ", capacity - " << v_empty.capacity() << std::endl;
+    std::vector<int> v_zero(5);
+    std::cout << "std | Fill constructor with default value: size - " << v_zero.size() << ", capacity - " << v_zero.capacity() 
+    << ", elements: " << v_zero[0] << " " << v_zero[1] << " " << v_zero[2] << " " << v_zero[3] << " " << v_zero[4] << std::endl;
+    std::vector<int> v_full(3, 42);
+    std::cout << "std | Fill constructor with value = 42: size - " << v_full.size() << ", capacity - " << v_full.capacity() 
+    << ", elements: " << v_full[0] << " " << v_full[1] << " " << v_full[2] << std::endl;
+    std::vector<int> v_range(v_full.begin(), v_full.end());
+    std::cout << "std | Range constructor (fill): size - " << v_range.size() << ", capacity - " << v_range.capacity() 
+    << ", elements: " << v_range[0] << " " << v_range[1] << " " << v_range[2] << std::endl;
+    std::vector<int> v_copy(v_full);
+    std::cout << "std | Copy constructor (fill): size - " << v_copy.size() << ", capacity - " << v_copy.capacity() 
+    << ", elements: " << v_copy[0] << " " << v_copy[1] << " " << v_copy[2] << std::endl;
+
+    ft::vector<std::string> ft_empty;
+    std::cout << "ft | Default constructor: size - " << ft_empty.size() << ", capacity - " << ft_empty.capacity() << std::endl;
+    std::vector<int> ft_zero(5);
+    std::cout << "ft | Fill constructor with default value: size - " << ft_zero.size() << ", capacity - " << ft_zero.capacity() 
+    << ", elements: " << ft_zero[0] << " " << ft_zero[1] << " " << ft_zero[2] << " " << ft_zero[3] << " " << ft_zero[4] << std::endl;
+    ft::vector<int> ft_full(3, 42);
+    std::cout << "ft | Fill constructor with value = 42: size - " << ft_full.size() << ", capacity - " << ft_full.capacity() 
+    << ", elements: " << ft_full[0] << " " << ft_full[1] << " " << ft_full[2] << std::endl;
+    // ft::vector<int> ft_range(ft_full.begin(), ft_full.end());
+    // std::cout << "ft | Range constructor (fill): size - " << ft_range.size() << ", capacity - " << ft_range.capacity() 
+    // << ", elements: " << ft_range[0] << " " << ft_range[1] << " " << ft_range[2] << std::endl;
+    // ft::vector<int> ft_copy(ft_full);
+    // std::cout << "ft | Copy constructor (fill): size - " << ft_copy.size() << ", capacity - " << ft_copy.capacity() 
+    // << ", elements: " << ft_copy[0] << " " << ft_copy[1] << " " << ft_copy[2] << std::endl;
+
+
+
+
+
     // std::cout << "++" << std::endl;
     // for(int i = 0; i != 18; i++)
     // {
@@ -17,15 +52,5 @@ int main()
     //     v.pop_back();
     //     std::cout << "SIZE: " << v.size() << " CAPACITY: " << v.capacity() << std::endl;
     // }
-
-    v.push_back("Blabla");
-    std::cout << v[0] << std::endl;
-
-    std::string *ptr = &v[0];
-
-    v.push_back("croak");
-    v.push_back("killkillkill");
-
-    std::cout << *ptr << std::endl;
 
 }
